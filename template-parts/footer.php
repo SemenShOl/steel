c9980ad8a8a6f5138bf15f02d929c55528ea730d
SemenShOl <semen.kursk.ru@gmail.com>
2024-04-12 20:30:52 +0300

    logo and bg footer change

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -5,7 +5,9 @@
 				<div class="col-2">
 					<div class="block-main-logo">
 						<a href="<?php echo home_url( '/' ); ?>">
-							<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/n_logo.pnh">
+							<!-- <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/n_logo.pnh"> -->
+							<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/n_Dark_logo.svg">
+
 						</a>
 					</div>
 				</div>
