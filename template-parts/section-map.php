17d3ea383984df1f8dc7618bb33cd3da82b0c409
SemenShOl <semen.kursk.ru@gmail.com>
2024-04-28 17:38:13 +0300

    map added

--- template-parts/section-map.php
+++ template-parts/section-map.php
@@ -1,8 +1,9 @@
 <section class="section-map" style="grid-area: map;">
-    <?php
-        $mapMarkers = '[';
-        $mapMarkers .= '{"coords": [55.026324, 82.917016], "type": "main", "iconSize": [80, 120], "iconOffset": [-40, 120], "iconUrl": "'.  esc_url(get_template_directory_uri()) . '/assets/images/map-marker.webp", "infoWindowText": "г. Новосибирск, ул. Советская, д.23, оф.403"}';
-        $mapMarkers .= ']';
+    <!-- <?php
+
+    // $mapMarkers = '[';
+    // $mapMarkers .= '{"coords": [55.026324, 82.917016], "type": "main", "iconSize": [80, 120], "iconOffset": [-40, 120], "iconUrl": "' . esc_url(get_template_directory_uri()) . '/assets/images/map-marker.webp", "infoWindowText": "г. Новосибирск, ул. Советская, д.23, оф.403"}';
+    // $mapMarkers .= ']';
     ?>
 
     <div
@@ -10,5 +11,9 @@
         data-map-marker='<?php echo $mapMarkers; ?>'
         data-map-center="<?php echo '[55.026324, 82.918516]'; ?>" 
         data-map-zoom="16"
-    ></div>
+    ></div> -->
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2285.2814669942973!2d82.89502227636038!3d55.055804845460166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x42dfe453a713c393%3A0xc83c3332ca56343e!2zMi3RjyDQodC-0Y7Qt9CwINCc0L7Qu9C-0LTQtdC20Lgg0YPQuy4sIDMxLCDQndC-0LLQvtGB0LjQsdC40YDRgdC6LCDQndC-0LLQvtGB0LjQsdC40YDRgdC60LDRjyDQvtCx0LsuLCA2MzAwODI!5e0!3m2!1sru!2sru!4v1714314796097!5m2!1sru!2sru"
+         style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
 </section>
